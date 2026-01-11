bee4d313c0ada7237a08ee1f7e5e5751bea53c47
George Pearce <gsp@gspearce.com>
2026-01-11 19:40:44 +0000

    Initial commit with Xcode project structure

---
+++ barcodescanner/barcodescanner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  barcodescanner
+//
+//  Created by George Pearce on 11/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
